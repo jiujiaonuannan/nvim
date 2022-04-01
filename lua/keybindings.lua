cb39b75a565161250addc7c1ebfdb21f554a7647
alvin.wang <alvin.wang@binance.com>
2022-04-01 22:34:57 +0800

    fix: commit bufferline

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -11,6 +11,17 @@ map("i", "<C-h>", "<ESC>I", opt)
 map("i", "<C-l>", "<ESC>A", opt)
 
 
+-- bufferline
+-- 左右Tab切换
+map("n", "<C-h>", ":BufferLineCyclePrev<CR>", opt)
+map("n", "<C-l>", ":BufferLineCycleNext<CR>", opt)
+-- 关闭
+--"moll/vim-bbye"
+map("n", "<C-w>", ":Bdelete!<CR>", opt)
+map("n", "<leader>bl", ":BufferLineCloseRight<CR>", opt)
+map("n", "<leader>bh", ":BufferLineCloseLeft<CR>", opt)
+map("n", "<leader>bc", ":BufferLinePickClose<CR>", opt)
+
 -- 插件快捷键
 local pluginKeys = {}
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -7,6 +7,8 @@ packer.startup(
 	 -------------------------- plugins -------------------------------------------
     -- nvim-tree (新增)
     use({ "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" })
+		-- bufferline (新增)
+    use({ "akinsho/bufferline.nvim", requires = { "kyazdani42/nvim-web-devicons", "moll/vim-bbye" }})
 
 	 -- tokyonight
 	 use("folke/tokyonight.nvim")
