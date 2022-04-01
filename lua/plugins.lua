bbb7edebbdbd242e598df79c53d1d198ada068f2
alvin.wang <alvin.wang@binance.com>
2022-04-01 21:14:15 +0800

    fix: commit other theme

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,7 +6,21 @@ packer.startup(
 
 	 -- tokyonight
 	 use("folke/tokyonight.nvim")
-
+	 -- OceanicNext
+	 use("mhartington/oceanic-next")
+	 -- gruvbox
+	 use({
+		 "ellisonleao/gruvbox.nvim",
+		 requires = {"rktjmp/lush.nvim"}
+	 })
+	 -- zephyr
+	 -- use("glepnir/zephyr-nvim")
+	 -- nord
+	 use("shaunsingh/nord.nvim")
+	 -- onedark
+	 use("ful1e5/onedark.nvim")
+	 -- nightfox
+	 use("EdenEast/nightfox.nvim")
    -- 你的插件列表...
 end)
 
