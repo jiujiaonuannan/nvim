2045b76d29b0125127c2417c46a13690ed1dc8a6
alvin.wang <alvin.wang@binance.com>
2022-04-02 21:05:13 +0800

    fix: commit telescope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -30,9 +30,13 @@ packer.startup(
 	 use("ful1e5/onedark.nvim")
 	 -- nightfox
 	 use("EdenEast/nightfox.nvim")
+	 -- telescope extensions
+	 use "LinArcX/telescope-env.nvim"
 
 	 -- nvim-tree (新增)
 	 use({ "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" })
+	 -- telescope （新增）
+	 use { 'nvim-telescope/telescope.nvim', requires = { "nvim-lua/plenary.nvim" } }
 end)
 
 -- 每次保存 plugins.lua 自动安装插件
