80c6b55f2c72c259c6673d63ab9e8ec4e9fdd820
alvin.wang <alvin.wang@binance.com>
2022-04-04 16:55:31 +0800

    chore: open fold module

--- lua/plugin-config/nvim-treesitter.lua
+++ lua/plugin-config/nvim-treesitter.lua
@@ -24,4 +24,16 @@ treesitter.setup({
 			scope_incremental = "<TAB>",
 		},
 	},
+
+	-- 启用代码缩进模块 (=)
+	indent = {
+		enable = true,
+	},
+
+	-- 开启 Folding 模块
+	vim.opt.foldmethod = "expr"
+	vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+	-- 默认不要折叠
+	-- https://stackoverflow.com/questions/8316139/how-to-set-the-default-to-unfolded-when-you-open-a-file
+	vim.opt.foldlevel = 99
 })
