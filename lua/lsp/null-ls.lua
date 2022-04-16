bd580ab21052e82dea97b128531b5233ad71ea2a
alvin.wang <alvin.wang@binance.com>
2022-04-16 10:20:13 +0800

    chore: add eslint auto fix action

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -5,6 +5,8 @@ if not status then
 end
 
 local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
   debug = false,
@@ -15,7 +17,7 @@ null_ls.setup({
     -- StyLua
     formatting.stylua,
     -- frontend
-    formatting.prettier.with({ -- 只比默认配置少了 markdown
+    formatting.prettier.with({ -- 比默认少了 markdown
       filetypes = {
         "javascript",
         "javascriptreact",
@@ -30,15 +32,26 @@ null_ls.setup({
         "yaml",
         "graphql",
       },
+      extra_filetypes = { "njk" },
       prefer_local = "node_modules/.bin",
     }),
-    -- formatting.fixjson,
-    -- formatting.black.with({ extra_args = { "--fast" } }),
+    -- Diagnostics  ---------------------
+    diagnostics.eslint.with({
+      prefer_local = "node_modules/.bin",
+    }),
+    -- code actions ---------------------
+    code_actions.gitsigns,
+    code_actions.eslint.with({
+      prefer_local = "node_modules/.bin",
+    }),
   },
-  -- 保存自动格式化
+  -- #{m}: message
+  -- #{s}: source name (defaults to null-ls if not specified)
+  -- #{c}: code (if available)
+  -- 提示格式： [eslint] xxx
+  diagnostics_format = "[#{s}] #{m}",
   on_attach = function(client)
-    if client.resolved_capabilities.document_formatting then
-      vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-    end
+    -- 自定义 :Format 命令
+    vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()']])
   end,
 })
